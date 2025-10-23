43ec2e18ca4cc295590ef8c3cae4ff0898369f73
SanguraKoded <sanguraj12@gmail.com>
2025-10-23 13:52:06 +0300

    Add MaxDepthSearch.java

---
+++ src/Trees/MaxDepthSearch.java
@@ -0,0 +1,21 @@
+package Trees;
+
+public class MaxDepthSearch {
+
+    public int maxDepth(Node root){
+        if(root==null){
+            return 0;
+        }
+        return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        MaxDepthSearch mds = new MaxDepthSearch();
+        System.out.println(mds.maxDepth(root));
+    }
+}
