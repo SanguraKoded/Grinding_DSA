e08f0b3fc5f0bdd3b296f385e9fb286e572cf6ca
SanguraKoded <sanguraj12@gmail.com>
2025-10-23 15:38:12 +0300

    Add Level Order Traversal

---
+++ src/Trees/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,36 @@
+package Trees;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeLevelOrderTraversal {
+    public List<List<Integer>> orderLevel (Node root){
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null){
+            return result;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while(queue.size()!=0){
+            int size = queue.size();
+            List<Integer> currentLevel = new ArrayList<>();
+            for(int i=0;i<size;i++){
+                Node currentNode = queue.remove();
+                currentLevel.add(currentNode.val);
+                if(currentNode.left != null){
+                    queue.add(currentNode.left);
+                }
+                if(currentNode.right != null){
+                    queue.add(currentNode.right);
+                }
+            }
+            result.add(currentLevel);
+        }
+        return result;
+    }
+
+
+}
