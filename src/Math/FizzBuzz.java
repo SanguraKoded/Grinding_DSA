ccee0f1726c092db193b963b87575d74470452c5
SanguraKoded <sanguraj12@gmail.com>
2025-10-28 11:43:38 +0300

    Add FizzBuzz.java

---
+++ src/Math/FizzBuzz.java
@@ -0,0 +1,28 @@
+package Math;
+import java.util.*;
+
+public class FizzBuzz {
+    public List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<>();
+        for(int i=1; i<=n; i++){
+            if(i%3==0 && i%5==0){
+                list.add("FizzBuzz");
+            }
+            else if(i%3==0){
+                list.add("Fizz");
+            }
+            else if(i%5==0){
+                list.add("Buzz");
+            }
+            else{
+                list.add(String.valueOf(i));
+            }
+        }
+        return list;
+    }
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        System.out.println(fizzBuzz.fizzBuzz(20));
+    }
+
+}
