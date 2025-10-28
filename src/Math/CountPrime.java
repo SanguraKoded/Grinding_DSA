8b4d14377128bbd12a7131908dd75ebd48d4ca3d
SanguraKoded <sanguraj12@gmail.com>
2025-10-28 16:44:21 +0300

    Add CountPrime.java

---
+++ src/Math/CountPrime.java
@@ -0,0 +1,32 @@
+package Math;
+
+import java.util.*;
+
+public class CountPrime {
+
+    public int countPrimes(int n) {
+        if(n<=2) return 0;
+        boolean [] isPrime = new boolean[n];
+        for(int i=2; i<n; i++){
+            isPrime[i] = true;
+        }
+        for(int i=2; i*i<n; i++){
+            if(isPrime[i]){
+                for(int j=i*i; j<n; j+=i){
+                    isPrime[j] = false;
+                }
+            }
+        }
+        int primeCounter = 0;
+        for(int i=2; i<n; i++){
+            if(isPrime[i]){
+                primeCounter++;
+            }
+        }
+        return primeCounter;
+    }
+    public static void main(String[] args) {
+        CountPrime countPrime = new CountPrime();
+        System.out.println(countPrime.countPrimes(30));
+    }
+}
